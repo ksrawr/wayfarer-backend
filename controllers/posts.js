654bcf973794fd0f1c3caf5eb2be3884c642b0f7
Ryan <ryansherring@gmail.com>
2020-03-05 14:00:33 -0800

    adds get route

--- controllers/posts.js
+++ controllers/posts.js
@@ -30,6 +30,19 @@ const createPost = (request, response) => {
   });
 };
 
+const getPosts = (req, res) => {
+  db.Post.find((err, posts) => {
+    console.log(posts);
+    if (err) return res.status(400).json(err);
+    const resObj = {
+      message: "posts gotted",
+      data: posts,
+      requestedAt: new Date().toLocaleString
+    }
+    res.json(resObj);
+  });
+};
+
 const editPost = (req, res) => {
   db.Post.findByIdAndUpdate(
     req.params.id,
@@ -66,5 +79,6 @@ const destroyPost = (req, res) => {
 module.exports = {
   createPost,
   editPost,
-  destroyPost
+  destroyPost,
+  getPosts
 };
--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,9 @@ const controller = require("../controllers");
 /* Create Post */
 router.post("/posts", controller.post.createPost);
 
+/* Get Posts*/
+router.get('/posts', controller.post.getPosts);
+
 /* Edit Post */
 router.put("/posts/:id", controller.post.editPost);
 
